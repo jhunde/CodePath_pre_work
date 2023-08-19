6f51961cd9d8b644d915ee73c27b93187bd917b8
Hunde Kelbessa <hundekelbessa@Hundes-Mac-mini.local>
2023-08-19 13:53:05 -0700

    added all the files

---
+++ CodePath_pre_workUITests/CodePath_pre_workUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CodePath_pre_workUITestsLaunchTests.swift
+//  CodePath_pre_workUITests
+//
+//  Created by Hunde Kelbessa on 8/16/23.
+//
+
+import XCTest
+
+final class CodePath_pre_workUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
